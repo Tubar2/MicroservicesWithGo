7c88ab2803e9a8bd0768aba81062957522b010f1
Ricardo Santos <ricardosantos@Ricardos-MacBook-Pro.local>
2020-03-30 00:16:43 -0300

    Create Server and gracefully end it

--- src/main.go
+++ src/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"BuildingMicroservicesWithGo_NicJackson/src/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"BuildingMicroservicesWithGo_NicJackson/src/handlers"
 )
 
 func main() {
@@ -16,5 +20,28 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe("localhost:9090", sm)
+	s := &http.Server{
+		Addr:         "localhost:9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }

090122bd198620ad4a49a118e7a1406f470b0469
Ricardo Santos <ricardosantos@Ricardos-MacBook-Pro.local>
2020-03-29 23:57:08 -0300

    Refactoring code

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"BuildingMicroservicesWithGo_NicJackson/src/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	http.ListenAndServe("localhost:9090", sm)
+}

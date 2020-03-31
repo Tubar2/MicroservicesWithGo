6000673e00e015b85c46477fcecf3e9bdb267508
Ricardo Santos <ricardosantos@Ricardos-MacBook-Pro-16.local>
2020-03-30 23:48:50 -0300

    Update to restful API

--- src/data/products.go
+++ src/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -15,24 +16,68 @@ type Product struct {
 	SKU         string  `json:"sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
-	DeletedOn   string  `json:"_"`
+	DeletedOn   string  `json:"-"`
 }
 
-//Products defines an array o type &Product
+// FromJSON decodes a product structured in JSON into usable text
+func (p *Product) FromJSON(r io.Reader) error {
+	dec := json.NewDecoder(r)
+	return dec.Decode(p)
+}
+
+// AddProduct appends product item to end of th list
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	p.CreatedOn = time.Now().UTC().String()
+	p.UpdatedOn = p.CreatedOn
+	productList = append(productList, p)
+}
+
+// UpdateProduct updates
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.UpdatedOn = time.Now().UTC().String()
+	productList[pos] = p
+	return nil
+}
+
+// ErrProductNotFound struct defines error
+var ErrProductNotFound = fmt.Errorf("Product not foun")
+
+// returns product iof compatible id
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if id == p.ID {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+// getNextID returns next ID for insertion at list
+func getNextID() int {
+	return (productList[len(productList)-1].ID) + 1
+}
+
+// Products defines an array o type &Product
 type Products []*Product
 
-//ToJSON mehtod defines encodes my databse to JSON
+// ToJSON mehtod encodes contents of databse to JSON
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
 
-//GetProducts returns database
+// GetProducts returns database
 func GetProducts() Products {
 	return productList
 }
 
-//Static Database
+// Static Database
 var productList = []*Product{
 	&Product{
 		ID:          1,
--- src/handlers/products.go
+++ src/handlers/products.go
@@ -4,36 +4,120 @@ import (
 	"BuildingMicroservicesWithGo_NicJackson/src/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
-//Products defines a handler for product
+// Products defines a handler for product
 type Products struct {
 	l *log.Logger
 }
 
-//NewProducts defines a function that returns a new Products object handler
+// NewProducts defines a function that returns a new Products object handler
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler
 func (p *Products) ServeHTTP(respW http.ResponseWriter, req *http.Request) {
-	//handle Get operation requests
+	// handle Get operation requests
 	if req.Method == http.MethodGet {
 		p.getProducts(respW, req)
 		return
 	}
 
-	//handle Put operation requests
+	// handle Post operation requests
+	if req.Method == http.MethodPost {
+		p.postProduct(respW, req)
+		return
+	}
 
-	//Catch all
+	// handle Put operation requests
+	if req.Method == http.MethodPut {
+		p.putProduct(respW, req)
+		return
+	}
+
+	//Catch all non implemented operations and send error
 	respW.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+// Implementation of the PUT operation on products
+// Updates databasse
+func (p *Products) putProduct(respW http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handling PUT operation")
+	r := regexp.MustCompile(`/([0-9]+)`)
+	g := r.FindAllStringSubmatch(req.URL.Path, -1)
+
+	if len(g) != 1 {
+		http.Error(respW, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+	if len(g[0]) != 2 {
+		http.Error(respW, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(g[0][1])
+	if err != nil {
+		http.Error(respW, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	// create empty product
+	prod := &data.Product{}
+
+	// decodes request json into empty product
+	err = prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(respW, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(respW, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(respW, "Product not found", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+// Implementation of the POST operation on product
+// Adds to databse
+func (p *Products) postProduct(respW http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handling POST operation")
+
+	// create empty product
+	prod := &data.Product{}
+
+	// decodes request json into empty product
+	err := prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(respW, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	// append new product into database
+	data.AddProduct(prod)
+
+}
+
+// Implementation of the GET operation on products
+// Retrieves database
 func (p *Products) getProducts(respW http.ResponseWriter, req *http.Request) {
+	p.l.Println("Handling GET operation")
+
+	// fetch products from databse
 	lp := data.GetProducts()
 
+	// parse fetched products to JSON
 	err := lp.ToJSON(respW)
 	if err != nil {
-		http.Error(respW, "Unable to marshal json", http.StatusInternalServerError)
+		http.Error(respW, "Unable to encode json", http.StatusInternalServerError)
+		return
 	}
 }
